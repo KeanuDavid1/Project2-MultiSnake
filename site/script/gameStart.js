71e2f82534cbe036456523a33178831c7c5caab1
Berend <berend.vandenbussche@student.howest.be>
2020-01-07 21:18:45 +0100

    Added comments to js files

--- site/script/gameStart.js
+++ site/script/gameStart.js
@@ -3,5 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   DOMCanvas = document.querySelector('.js-game-canvas');
   initGameArea(DOMCanvas);
   simpleGameMode();
+
+  //Hier roep ik de methode aan die het voedsel laat spawnen (generateFood.js)
   setInterval(generateFood, Math.floor(randomInterval));
 });
--- site/script/updateFrame.js
+++ site/script/updateFrame.js
@@ -1,16 +1,29 @@
 function updateGameArea() {
+  //Verwijdert alles dat op canvas getekend is.
+  //Zit in gameArea.js
   gameArea.clear();
 
+  //Overloopt alle snakes
   for (snake of snakeArray) {
+    //Tekent de snake onderdelen opnieuw
+    //Functie zit in snakeObject.js
     snake.update();
+    //Checkt of de snake-head out of bounds is.
+    //SnakePieces[0] verwijst naar de Snake head (de eerste part van snake)
+    //isOutOfBounds functie zit in headComponent.js
     if (snake.snakePieces[0].isOutOfBounds()) {
+      //Om de tekst te maken als je het spel verliest.
       ctx = gameArea.context;
       ctx.font = '30px Arial';
       ctx.fillStyle = 'red';
       ctx.fillText('Get rekt', 200, 150);
+
+      //Stopt de refresh functie en dus heel het spel.
+      //Zie gameArea.js
       gameArea.stop();
     }
   }
+  // Zorgt dat enkel het stuk fruit die random gekozen werd in generateFood.js geupdate wordt
   if (foodstate == 0) {
     apple.update();
   } else if (foodstate == 1) {
