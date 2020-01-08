01a106fecf1fb47202ad3171cc59d7c19f078d45
Berend <berend.vandenbussche@student.howest.be>
2020-01-08 20:16:49 +0100

    Game: Snake grows when eating healty foods, when eating unhealty foods the canvas shows -5 points

--- site/script/foodObjects.js
+++ site/script/foodObjects.js
@@ -19,4 +19,24 @@ function component(width, height, color, x, y, type) {
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
   };
+  this.eatFood = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + this.width;
+    var mytop = this.y;
+    var mybottom = this.y + this.height;
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + otherobj.width;
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + otherobj.height;
+    var crash = true;
+    if (
+      mybottom < othertop ||
+      mytop > otherbottom ||
+      myright < otherleft ||
+      myleft > otherright
+    ) {
+      crash = false;
+    }
+    return crash;
+  };
 }
