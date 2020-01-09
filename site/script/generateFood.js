247a4aa30217326d846c57ddef0e07f87a99906a
Berend <berend.vandenbussche@student.howest.be>
2020-01-09 11:43:44 +0100

    Game: Minor fixes

--- site/script/generateFood.js
+++ site/script/generateFood.js
@@ -2,34 +2,32 @@
 const generateFood = function() {
   let food;
   let healthy;
-  let whichFood = Math.floor(Math.random() * 4);
+  let whichFood = Math.round(Math.random() * 4);
   if (whichFood == 0) {
-    food = "Apple"
+    food = 'Apple';
     healthy = true;
-  }
-  else if(whichFood == 1){
-    food = "Pear"
+  } else if (whichFood == 1) {
+    food = 'Pear';
     healthy = true;
-  }
-  else if(whichFood == 2){
-    food = "Carrot"
+  } else if (whichFood == 2) {
+    food = 'Carrot';
     healthy = true;
-  }
-  else if(whichFood == 3){
-    food = "Burger"
+  } else if (whichFood == 3) {
+    food = 'Burger';
+    healthy = false;
+  } else if (whichFood == 4) {
+    food = 'Pizza';
     healthy = false;
   }
-  else if(whichFood == 4){
-    food = "Pizza"
-    healthy = false;
-  }
-  foodArray.push(new component(
-    foodSize,
-    foodSize,
-    `img/${food}.svg`,
-    Math.random() * canvasWidth,
-    Math.random() * canvasHeight,
-    'image', healthy
-  ));
-
+  foodArray.push(
+    new component(
+      foodSize,
+      foodSize,
+      `img/${food}.svg`,
+      Math.random() * canvasWidth,
+      Math.random() * canvasHeight,
+      'image',
+      healthy
+    )
+  );
 };
