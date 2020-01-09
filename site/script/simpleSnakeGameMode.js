9696dc8264f5bb6692bd922051b5ed858b931037
Berend <berend.vandenbussche@student.howest.be>
2020-01-09 11:58:47 +0100

    Game: Minor Changes

--- site/script/simpleSnakeGameMode.js
+++ site/script/simpleSnakeGameMode.js
@@ -1,15 +1,16 @@
 //Hier komen alle condities voor deze gamemode in, spawnen van 1 snake, start direction, en keydown event listener.
 
-const simpleGameMode = function () {
-    //Spawn snake head
-    snakeArray.push(new snakeObject(100, 100, 0));
-    //Voegt 1 body part toe
-    snakeArray[0].addNewPiece();
-    //Bepaalt de start direction
-    // snakeArray[0].snakePieces[0].direction = "up";
-    snakeArray[0].snakePieces[0].moveup();
-    //Luistert naar de key presses
-    document.addEventListener('keydown', keypressed);
-    setInterval(generateFood, 6000);
-}
-
+const simpleGameMode = function() {
+  //Spawn snake head
+  snakeArray.push(new snakeObject(100, 100, 0));
+  //Voegt 1 body part toe
+  snakeArray[0].addNewPiece();
+  //Spawnt 1 voedsel stukje
+  generateFood();
+  //Bepaalt de start direction
+  // snakeArray[0].snakePieces[0].direction = "up";
+  snakeArray[0].snakePieces[0].movedown();
+  //Luistert naar de key presses
+  document.addEventListener('keydown', keypressed);
+  setInterval(generateFood, 6000);
+};
