0fc376b819e4d018aa53adb5d6ddeef39e1833cc
Berend <berend.vandenbussche@student.howest.be>
2020-01-14 15:28:09 +0100

    game: Styling fixed

--- site/script-design/button-functionality.js
+++ site/script-design/button-functionality.js
@@ -1,19 +1,14 @@
+const init = function() {
+  document.querySelector('#gamestart').addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
 
-const init = function(){
-    document.querySelector("#gamestart").addEventListener("click", function(){
-        window.location.href = "game.html";
-    });
-
-    document.querySelector("#scoreboard").addEventListener("click", function(){
-        window.location.href = "#";
-    });
-
-    document.querySelector("#options").addEventListener("click", function(){
-        window.location.href = "#";
-    });
-}
+  document.querySelector('#scoreboard').addEventListener('click', function() {
+    window.location.href = '#';
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.info('DOM geladen');
-    init();
+  console.info('DOM geladen');
+  init();
 });
