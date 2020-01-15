db2661576574d9141e49e3756b424ddfb00125fa
Szymon SZymczak <szymon.szymczak@student.howest.be>
2020-01-15 12:48:41 +0100

    Added edge/IE support

--- site/script/movement.js
+++ site/script/movement.js
@@ -1,48 +1,48 @@
 //Dit is de callback bij een key press event, we kijken naar welk toets er op geklikt werd en binden acties eraan.
 
 const keypressed = function () {
-    console.log(event.code);
-    if (event.code == "ArrowUp") {
+    console.log(event.key);
+    if (event.key == "ArrowUp") {
         if (snakeArray[0].snakePieces[0].direction != "down") {
             snakeArray[0].snakePieces[0].moveup();
         }
     }
-    else if (event.code == "ArrowDown") {
+    else if (event.key == "ArrowDown") {
         if (snakeArray[0].snakePieces[0].direction != "up") {
             snakeArray[0].snakePieces[0].movedown();
         }
     }
-    else if (event.code == "ArrowLeft") {
+    else if (event.key == "ArrowLeft") {
         if (snakeArray[0].snakePieces[0].direction != "right") {
             snakeArray[0].snakePieces[0].moveleft();
         }
     }
-    else if (event.code == "ArrowRight") {
+    else if (event.key == "ArrowRight") {
         if (snakeArray[0].snakePieces[0].direction != "left") {
             snakeArray[0].snakePieces[0].moveright();
         }
     }
-    if (event.code == "KeyW") {
+    if (event.key == "w") {
         if (snakeArray[1].snakePieces[0].direction != "down") {
             snakeArray[1].snakePieces[0].moveup();
         }
     }
-    else if (event.code == "KeyS") {
+    else if (event.key == "s") {
         if (snakeArray[1].snakePieces[0].direction != "up") {
             snakeArray[1].snakePieces[0].movedown();
         }
     }
-    else if (event.code == "KeyA") {
+    else if (event.key == "a") {
         if (snakeArray[1].snakePieces[0].direction != "right") {
             snakeArray[1].snakePieces[0].moveleft();
         }
     }
-    else if (event.code == "KeyD") {
+    else if (event.key == "d") {
         if (snakeArray[1].snakePieces[0].direction != "left") {
             snakeArray[1].snakePieces[0].moveright();
         }
     }
-    else if (event.code == "Space") {
+    else if (event.key == "Space") {
         snakeArray[0].addNewPiece();
     }
 }
