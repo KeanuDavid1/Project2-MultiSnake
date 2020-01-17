bda1a9bef08f156502c48a192c04da1d72402bb4
Keanu David <keanu.david@student.howest.be>
2020-01-17 14:30:03 +0100

    Added script & DB class

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,19 @@
-x = 1
-y = 2
+import threading
+import time
+from subprocess import check_output
 
-sum = x + y
-print(sum)
+import serial
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_socketio import SocketIO
+from RPi import GPIO
+
+
+@socketio.on("connect")
+def connecting():
+    socketio.emit("connect")
+    print("Connection with client established")
+
+
+if __name__ == '__main__':
+    socketio.run(main, host="0.0.0.0", port="5000")
