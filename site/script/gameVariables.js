253d0d161300d28d3ef8d7cc91a20464b6481232
Szymon SZymczak <szymon.szymczak@student.howest.be>
2020-01-17 10:23:30 +0100

    Deleted statisticsVariables.js and moved the variables to gameVariables.js

--- site/script/gameVariables.js
+++ site/script/gameVariables.js
@@ -9,12 +9,14 @@ let firstItem = true;
 let rockCounter = 0;
 let itemArray = [];
 let textArray = [];
+let gameStartTime;
+let gameEndTime;
 
 // SETTINGS
 
 // dit houd de game settings bij
 // die meegegeven worden vanuit de settings page
-let gameSettings = {mode: 0, difficulty: 0, players: 1};
+let gameSettings = { mode: 0, difficulty: 0, players: 1 };
 let players = 0;
 let maxPlayers = 4;
 let difficulties = ['Makkelijk', 'Normaal', 'Moeilijk'];
