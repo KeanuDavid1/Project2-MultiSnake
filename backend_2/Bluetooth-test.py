358581a26e00caa45771ee246ae90ffd0b2a201d
Szymon SZymczak <szymon.szymczak@student.howest.be>
2020-01-28 11:35:44 +0100

    Added test ble scan file

--- backend_2/Bluetooth-test.py
+++ backend_2/Bluetooth-test.py
@@ -4,7 +4,11 @@ import time
 from models.HRM import HRM
 
 
+sc = bluepy.btle.Scanner()
+scan_results = sc.scan(10)
 
+for scan in scan_results:
+    print("{} | {}".format(scan.getScanData(),scan.addr))
 
 
 
