7768e4c02bb0cd9430b02783138f30554d959712
Keanu David <keanu.david@student.howest.be>
2020-01-29 12:20:17 +0100

    Added comments & fixed typos

--- site/script/gameVariables.js
+++ site/script/gameVariables.js
@@ -61,12 +61,12 @@ let tutorialTitles = [
 let tutorialText = [
   'Wanneer je een gezond stukje voedsel eet krijg je 10 punten, deze punten bepalen hoe hoof je aan het einde van het spel op het scorebord komt te staan. Naast de 10 punten die je krijgt zal je slang ook 1 blokje langer worden.',
   'Wanneer je een ongezond stukje voedsel eet zullen er 50 punten worden afgetrokken van je huidige score, ook wanneer je minder dan 50 punten hebt! Let dus goed op en probeer enkel gezond voedsel te eten.',
-  'Wanneer je slang met een obstakel op het speelveld botst verlies je een leven, hierna word je voor een aantal seconden imuun voor obstakels, andere slangen & de randen van het speelveld zodat je je slang naar een veilige plaats op het speelveld kunt navigeren. Let op, je bent niet lang imuun!',
+  'Wanneer je slang met een obstakel op het speelveld botst verlies je een leven, hierna word je voor een aantal seconden immuun voor obstakels, andere slangen & de randen van het speelveld zodat je je slang naar een veilige plaats op het speelveld kunt navigeren. Let op, je bent niet lang immuun!',
   'Wanneer je tegen de randen van het speelveld botst verlies je een leven en zal je slang in een willekeurige richting verder bewegen.',
   'Wanneer 2 slangen met elkaar botsen hoofd tegen hoofd dan verliezen beide slangen een leven.',
   'Wanneer een slang tegen een andere slang zijn staart bots zal er een leven afgetrokken worden van de slang die botste.',
   'Wanneer je geen levens meer hebt verdwijnt je slang van het speelveld en wordt het vakje met je naam en punten grijs gekleurd. Wanneer je als eerste dood was kan je nog altijd winnen als je de meeste punten had.',
-  'Als je in de attack spelmodus speelt is het de bedoeling om zo veel mogelijk te bewegen en dus de hoogste hartslag te hebben, de persoon/slang met de hoogste hartslag is imuun voor obstakels, de randen van het speelveld en andere slangen. De slang met de hoogste hartslag kan dus jagen op de andere slangen op het spelbord. De slang met de hoogste hartslag zal zwart worden.'
+  'Als je in de attack spelmodus speelt is het de bedoeling om zo veel mogelijk te bewegen en dus de hoogste hartslag te hebben, de persoon/slang met de hoogste hartslag is immuun voor obstakels, de randen van het speelveld en andere slangen. De slang met de hoogste hartslag kan dus jagen op de andere slangen op het spelbord. De slang met de hoogste hartslag zal zwart worden.'
 ];
 
 const socketIP = 'http://192.168.0.252' + ':5000';
